3b209570aa511487784074c17b510d3e1290b0cd
Maksim Kalugin <gglolgg889@gmail.com>
2025-01-31 13:35:33 +0500

    refactoring

--- converter.py
+++ converter.py
@@ -1,6 +1,6 @@
 import openpyxl
 from datetime import datetime
-import re 
+import re
 
 class TestPoint:
     def __init__(self, test_date, org_name, sex, ind_num, name, birth_date, locus_dic, conclusion):
@@ -13,28 +13,16 @@ class TestPoint:
         self.locus_dic = locus_dic
         self.conclusion = conclusion
 
-name = (3, 5)
-date = (4, 5)
-start_header_row = 5
-locus_head_row = start_header_row + 1
-start_locus_col = 6
-start_data_row = 7
-output_header_row = 5
-output_locus_start_col = 10
-output_locus_end_col = 41
-#output_locus_sequence = ["BM1818", "BM1824", "BM2113", "CYP21", "ETH10", "ETH225", "ETH3", "INRA023", "RM067", "SPS115", "TGLA126", "TGLA122", "TGLA227", "TGLA53", "MGTG4B", "SPS113"]
-
-def create_dic(sheet, row, locus_count):
+def create_dic(sheet, row, locus_count, start_locus_col, locus_head_row):
     res_dic = {}
-   
     for col in range(start_locus_col, start_locus_col + locus_count):
         res_dic[sheet.cell(locus_head_row, col).value] = [sheet.cell(row, col).value, sheet.cell(row + 1, col).value]
     return res_dic
 
-
-
 def conclusion_calculate(input_string):
-
+    if input_string is None:
+        return "Ошибка: Невозможно обработать входную строку"
+    
     input_string = input_string.lower()  # ignore case
 
     if "отец соответствует" in input_string and "мать не тестирована" in input_string:
@@ -44,21 +32,19 @@ def conclusion_calculate(input_string):
     elif "отец соответствует" in input_string and "мать соответствует" in input_string:
         return "да/да"
     elif "отец не соответствует" in input_string or "отец не тестирован" in input_string and "мать не тестирована" in input_string or "мать не соответствует" in input_string:
-        return "нет/нет"  #Assuming if father doesn't match and mother isn't tested, we conclude mother also doesn't match.  This could be adjusted based on specific requirements.
-    elif "отец не соответствует" in input_string or "отец не тестирован" and "мать соответствует" in input_string:
+        return "нет/нет"
+    elif "отец не соответствует" in input_string or "отец не тестирован" in input_string and "мать соответствует" in input_string:
         return "нет/да"
     elif "родители соответствуют" in input_string or 'родители сответствуют' in input_string:
         return "да/да"
     elif "получен микросателлитный профиль" in input_string:
-        return "тест" #Indicates a successful test, not a parentage conclusion.
+        return "тест"
     elif "получен микросател- литный профиль" in input_string:
         return "тест"
     else:
         return input_string
 
-    
-        
-def write(test_point, output_sheet, counter, main_counter,  output_locus_names):
+def write(test_point, output_sheet, counter, main_counter, output_locus_names, output_header_row, output_locus_start_col, output_locus_end_col):
     output_sheet.cell(main_counter + counter + output_header_row, 1, main_counter + counter)
     output_sheet.cell(main_counter + counter + output_header_row, 2, test_point.org_name)
     output_sheet.cell(main_counter + counter + output_header_row, 3, test_point.test_date)
@@ -79,7 +65,7 @@ def write(test_point, output_sheet, counter, main_counter,  output_locus_names):
             output_sheet.cell(main_counter + counter + output_header_row, output_locus_start_col + i * 2 + 1, "")
     output_sheet.cell(main_counter + counter + output_header_row, output_locus_end_col + 1, test_point.conclusion)
 
-def process_excel_iterative(input_file, output_file, main_counter):
+def process_excel_iterative(input_file, output_file, main_counter, config):
     counter = 0
     input_wb = openpyxl.load_workbook(input_file, data_only=True)
     input_sheet = input_wb.active
@@ -87,24 +73,24 @@ def process_excel_iterative(input_file, output_file, main_counter):
     output_wb = openpyxl.load_workbook(output_file)
     output_sheet = output_wb.active
 
-    org_name = input_sheet.cell(name[0], name[1]).value
-    testing_date = input_sheet.cell(date[0], date[1]).value
+    org_name = input_sheet.cell(config['name'][0], config['name'][1]).value
+    testing_date = input_sheet.cell(config['date'][0], config['date'][1]).value
 
-    output_locus_names = [output_sheet.cell(output_header_row, col).value for col in range(output_locus_start_col, output_locus_end_col, 2)]
-    col = start_locus_col
+    output_locus_names = [output_sheet.cell(config['output_header_row'], col).value for col in range(config['output_locus_start_col'], config['output_locus_end_col'], 2)]
+    col = config['start_locus_col']
     locus_count = 0
-    while input_sheet.cell(locus_head_row, col).value != None:
+    while input_sheet.cell(config['locus_head_row'], col).value != None:
         locus_count += 1    
         col += 1
-    for row in range(7, input_sheet.max_row + 1, 2):
+    for row in range(config['start_data_row'], input_sheet.max_row + 1, 2):
         sex = input_sheet.cell(row, 2).value
         cell_value = input_sheet.cell(row, 1).value
         if sex is None:
             sex = "м"
-        if cell_value is None or not re.match(r'^\d+/24 *днк$', cell_value.lower()) or sex not in "пм" :
+        if cell_value is None or not re.match(r'^\d+/24 *днк$', cell_value.lower()) or sex not in "пм":
             continue
-        locus_dic = create_dic(input_sheet, row, locus_count)
-        input_end_locus_col = len(locus_dic.keys())+ start_locus_col -1
+        locus_dic = create_dic(input_sheet, row, locus_count, config['start_locus_col'], config['locus_head_row'])
+        input_end_locus_col = len(locus_dic.keys()) + config['start_locus_col'] - 1
         test_name = input_sheet.cell(row, 3).value
         ind_num = input_sheet.cell(row, 4).value
         birth_date = input_sheet.cell(row, 5).value
@@ -112,12 +98,12 @@ def process_excel_iterative(input_file, output_file, main_counter):
         if input_sheet.cell(row, input_end_locus_col + work_cell_con).value == None:
             work_cell_con += 1
         con_val = input_sheet.cell(row, input_end_locus_col + work_cell_con).value
-        if(con_val == None):
-             conclusion = "" #Надо исправить потом
+        if con_val == None:
+            conclusion = ""  # Надо исправить потом
         else:
             conclusion = conclusion_calculate(con_val)
         test_point = TestPoint(testing_date, org_name, sex, ind_num, test_name, birth_date, locus_dic, conclusion)
-        write(test_point, output_sheet, counter, main_counter, output_locus_names)
+        write(test_point, output_sheet, counter, main_counter, output_locus_names, config['output_header_row'], config['output_locus_start_col'], config['output_locus_end_col'])
         counter += 1
 
     output_wb.save(output_file)
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from converter import *
-
 import os
 from converter import process_excel_iterative
 
-def find_and_process_files(base_dir, output_file):
-    main_counter = 1
+def find_and_process_files(base_dir, output_file, main_counter, config):
     for root, dirs, files in os.walk(base_dir):
         if 'ДНК' in dirs:
             dnk_dir = os.path.join(root, 'ДНК')
@@ -13,17 +10,22 @@ def find_and_process_files(base_dir, output_file):
                     if file.startswith('Заключение') and file.endswith('.xlsx'):
                         input_file = os.path.join(dnk_root, file)
                         print(f"Processing file: {input_file}")
-                        main_counter = process_excel_iterative(input_file, output_file, main_counter)
-                        break
-                break
-        else:
-            for file in files:
-                if file.startswith('Заключение') and file.endswith('.xlsx'):
-                    input_file = os.path.join(root, file)
-                    print(f"Processing file: {input_file}")
-                    main_counter = process_excel_iterative(input_file, output_file, main_counter)
+                        main_counter = process_excel_iterative(input_file, output_file, main_counter, config)
+    return main_counter
 
 if __name__ == "__main__":
     base_dir = r"C:\Users\gglol\Documents\ДНК Договора 2024 тестовый"
     output_file = r"C:\Users\gglol\Documents\ДНК Договора 2024 тестовый\Приложение 2.xlsx"
-    find_and_process_files(base_dir, output_file)
+    config = {
+        'name': (3, 5),
+        'date': (4, 5),
+        'start_header_row': 5,
+        'locus_head_row': 6,
+        'start_locus_col': 6,
+        'start_data_row': 7,
+        'output_header_row': 5,
+        'output_locus_start_col': 10,
+        'output_locus_end_col': 41
+    }
+    main_counter = 0
+    main_counter = find_and_process_files(base_dir, output_file, main_counter, config)
